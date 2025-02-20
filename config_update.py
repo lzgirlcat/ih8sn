39dda4ba102671135964a305ccf991c32922680a
lzgirlcat <lz@girlc.at>
2025-02-20 20:37:11 +0100

    update config_updater script

--- config_update.py
+++ config_update.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python3
 
 import urllib.request
+import json
 import re
 
 # Download the file
-url = "https://raw.githubusercontent.com/crdroidandroid/android_frameworks_base/14.0/core/java/com/android/internal/util/crdroid/PixelPropsUtils.java"
+url = "https://raw.githubusercontent.com/crdroidandroid/android_vendor_certification/refs/heads/15.0/gms_certified_props.json"
 response = urllib.request.urlopen(url)
-content = response.read().decode()
+content = json.loads(response.read())
 
-# Find the spoofBuildGms function
-spoof_build_gms = re.search(r'private static void spoofBuildGms\(\) \{(.+?)\}', content, re.DOTALL).group(1)
-
-# Extract the FINGERPRINT and DEVICE values
-fingerprint = re.search(r'setPropValue\("FINGERPRINT", "(.+?)"\);', spoof_build_gms).group(1)
-device = re.search(r'setPropValue\("DEVICE", "(.+?)"\);', spoof_build_gms).group(1)
 
 # Update the ih8sn.conf file
 with open('system/etc/ih8sn.conf', 'r') as file:
     config = file.read()
 
-config = re.sub(r'(?<=BUILD_FINGERPRINT=).*', fingerprint, config)
-config = re.sub(r'(?<=PRODUCT_NAME=).*', device, config)
+config = re.sub(r'(?<=BUILD_FINGERPRINT=).*', content["FINGERPRINT"], config)
+config = re.sub(r'(?<=PRODUCT_NAME=).*', content["DEVICE"], config)
 
 with open('system/etc/ih8sn.conf', 'w') as file:
     file.write(config)
 
 # Copy the file
-with open(f'system/etc/configs/ih8sn.conf.{device}', 'w') as file:
+with open(f'system/etc/configs/ih8sn.conf.{content["DEVICE"]}', 'w') as file:
     file.write(config)
